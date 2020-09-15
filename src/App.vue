21fafc59cb3d87a3c66c23df182cfcbe9d049f8f
Matheus Caetano <caetanomatheus876@gmail.com>
2020-09-15 10:57:18 -0300

    Added transition when switching pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="app">
-    <router-view/>
+    <transition name="fader">
+      <router-view/>
+    </transition>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.fader-enter-active, .fader-leave-active {
+  transition: .3s;
+}
+
+.fader-enter, .fader-leave-to {
+  filter: blur(10px);
+  opacity: 0;
+}
+</style>
