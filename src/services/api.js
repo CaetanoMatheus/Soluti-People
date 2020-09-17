e8a2f2b2ade5d65d0eac61b7f84cef0dcf2d9184
Matheus Caetano <caetanomatheus876@gmail.com>
2020-09-16 22:09:41 -0300

    Removed verifyAuthentication function from api code

--- src/services/api.js
+++ src/services/api.js
@@ -4,13 +4,6 @@ const token = window.localStorage.getItem('token')
 
 export const headers = { headers: { Authorization: `Bearer ${token}` } }
 
-export function verifyAuthentication(status) {
-  if (status === 401) {
-    window.localStorage.removeItem('token')
-    document.location.reload()
-  }
-}
-
 export default axios.create({
   baseURL: process.env.VUE_APP_API_URL,
 })
